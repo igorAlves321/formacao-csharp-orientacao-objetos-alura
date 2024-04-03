8077e1090090265913cabec3dc75bfaa3096ccbb
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-04-03 10:26:38 -0300

    feito segundo exercicio, e terceiro também, onde precisavamos criar uma lista e imprimir determinado indice na tela.

---
+++ exercicios/lista um da formação/Ex02ListasDeLinguagens/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02ListasDeLinguagens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+            List<string> listaDeLinguagens = new List<string> { "C#", "JS", "Java" };
+            //Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+            Console.WriteLine(listaDeLinguagens[0]);
+        }
+    }
+}
