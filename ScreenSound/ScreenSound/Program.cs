bb499de1a1e02ece15042b2a59a9c34401218eca
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-04-17 16:20:19 -0300

    refatorando um pouco o código e adcionando construtores.

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,15 +1,26 @@
-﻿Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A night at the opera";
+﻿using ScreenSound;
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of my life";
-musica1.Duracao = 231;
+Banda queen = new Banda("Queen");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Duracao = 354;
+Album albumDoQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
 
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
 albumDoQueen.ExibirMusicasDoAlbum();
+queen.ExibirDiscografia();
